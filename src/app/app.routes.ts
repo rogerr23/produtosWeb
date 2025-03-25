57624335f68432d998f7df83ea033ee1f5126417
rogerr23 <roger17ribeiro09@gmail.com>
2025-03-25 18:43:19 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { CadastroProdutosComponent } from './components/pages/cadastro-produtos/cadastro-produtos.component';
+import { ConsultarProdutosComponent } from './components/pages/consultar-produtos/consultar-produtos.component';
+import { EdicaoProdutosComponent } from './components/pages/edicao-produtos/edicao-produtos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'pages/cadastro-produtos',
+        component: CadastroProdutosComponent
+    },
+
+    {
+        path: 'pages/consultar-produtos',
+        component: ConsultarProdutosComponent
+    },
+
+    {
+        path: 'pages/edicao-produtos',
+        component: EdicaoProdutosComponent
+    }
+];
---
+++ src/app/components/pages/consultar-produtos/consultar-produtos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consultar-produtos',
+  imports: [],
+  templateUrl: './consultar-produtos.component.html',
+  styleUrl: './consultar-produtos.component.css'
+})
+export class ConsultarProdutosComponent {
+
+}
